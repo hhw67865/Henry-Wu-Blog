0b28f0ed46c30d2740b624ecf778c93cb719661c
Henry Wu <hhw67865@gmail.com>
2022-10-12 21:57:55 -0400

    added object in data.js

---
+++ data.js
@@ -0,0 +1,18 @@
+const myBlogs = [
+    {
+        title: "Command Line Interface(CLI)",
+        picture: "CLI.png",
+        post: "asd"
+    },
+    {
+        title: "Java",
+        picture: "Java.png",
+        post: "asd"
+    },
+    {
+        title: "HTML",
+        picture: "HTML.png",
+        post: "asd"
+    }
+    ];
+    
