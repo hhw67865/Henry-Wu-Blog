a6d09564c133a865f0a6665b77795fb391f92b1b
Henry Wu <hhw67865@gmail.com>
2022-10-13 09:03:05 -0400

    deleted previous information, fixed issue where button wasn't working. Everything corrected and set up

--- data.js
+++ data.js
@@ -18,3 +18,47 @@ const myBlogs = [
 
 
 
+    createNewPost(myBlogs);
+
+
+    function createNewPost(array) {    
+
+        for (let i = 0; i < array.length; i++) {
+            
+            const prev = document.querySelector(".prev");
+            const slideshowContainer = document.querySelector(".slideshow-container");
+            const mySlidesFade = document.createElement("div");
+            mySlidesFade.className = "mySlides fade";
+            const numbertext = document.createElement("div");
+            numbertext.className = "numbertext";
+            numbertext.innerText = `${i+1} / ${array.length}`
+            const image = document.createElement("img");
+            image.src = array[i].picture;
+            image.alt = array[i].picture;
+            image.style.width = "100%";
+            image.style.height = "100%";
+            const text = document.createElement("div");
+            text.className = "text";
+            text.innerText = array[i].title;
+    
+            slideshowContainer.insertBefore(mySlidesFade, prev);
+            mySlidesFade.append(numbertext, image, text);
+    
+            const dotHolder = document.querySelector(".dotHolder");
+            dotHolder.innerHTML += `
+            <span class="dot" onclick="currentSlide(${i+1})"></span>`
+        
+            const box = document.querySelector(".box");
+            const informationFade = document.createElement("div");
+            informationFade.className = "information fade";
+            const h3 = document.createElement("h3");
+            h3.innerText = array[i].title;
+            const p = document.createElement("p");
+            p.innerText = array[i].post;
+    
+            informationFade.append(h3, p);
+            box.append(informationFade);
+    
+        }
+    
+    }
