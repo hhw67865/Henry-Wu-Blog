0b77961ff2b367a235ea30c96d667e8e260fe913
Henry Wu <hhw67865@gmail.com>
2022-10-17 08:53:48 -0400

    added createOnePost

--- data.js
+++ data.js
@@ -18,10 +18,10 @@ const myBlogs = [
 
 
 
-    createNewPost(myBlogs);
+    createAllPost(myBlogs);
 
 
-    function createNewPost(array) {    
+    function createAllPost(array) {    
 
         for (let i = 0; i < array.length; i++) {
             
@@ -62,3 +62,43 @@ const myBlogs = [
         }
     
     }
+
+    function createOnePost(object) {
+        
+        const prev = document.querySelector(".prev");
+        const slideshowContainer = document.querySelector(".slideshow-container");
+        const mySlidesFade = document.createElement("div");
+        mySlidesFade.className = "mySlides fade";
+        const numbertext = document.createElement("div");
+        numbertext.className = "numbertext";
+        let slides = document.getElementsByClassName("mySlides");
+        numbertext.innerText = `${slides.length} / ${slides.length}`
+        const image = document.createElement("img");
+        image.src = object.picture;
+        image.alt = object.picture;
+        image.style.width = "100%";
+        image.style.height = "100%";
+        const text = document.createElement("div");
+        text.className = "text";
+        //text.innerText = object.title; Repetitive currently.
+
+        slideshowContainer.insertBefore(mySlidesFade, prev);
+        mySlidesFade.append(numbertext, image, text);
+
+        const dotHolder = document.querySelector(".dotHolder");
+        dotHolder.innerHTML += `
+        <span class="dot" onclick="currentSlide(${slides.length})"></span>`
+    
+        const box = document.querySelector(".box");
+        const informationFade = document.createElement("div");
+        informationFade.className = "information fade";
+        const h3 = document.createElement("h3");
+        h3.innerText = object.title;
+        const p = document.createElement("p");
+        p.innerText = object.post;
+
+        informationFade.append(h3, p);
+        box.append(informationFade);     
+    }
+
+    
